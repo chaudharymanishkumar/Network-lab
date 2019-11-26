9b6e4020e5b9c2fe87dace045bf2b982cd59df12
Manish Kumar Chaudhary <42311706+chaudharymanishkumar@users.noreply.github.com>
2019-11-26 12:08:51 +0530

    Add files via upload

---
+++ mono.py
@@ -0,0 +1,39 @@
+monoalpha_cipher = {
+    'a': 'm',
+    'b': 'n',
+    'c': 'b',
+    'd': 'v',
+    'e': 'c',
+    'f': 'x',
+    'g': 'z',
+    'h': 'a',
+    'i': 's',
+    'j': 'd',
+    'k': 'f',
+    'l': 'g',
+    'm': 'h',
+    'n': 'j',
+    'o': 'k',
+    'p': 'l',
+    'q': 'p',
+    'r': 'o',
+    's': 'i',
+    't': 'u',
+    'u': 'y',
+    'v': 't',
+    'w': 'r',
+    'x': 'e',
+    'y': 'w',
+    'z': 'q',
+    ' ': ' ',
+}
+inverse_monoalpha_cipher = {}
+for key, value in monoalpha_cipher.iteritems():
+    inverse_monoalpha_cipher[value] = key
+message = raw_input("Enter the text:-")
+
+encrypted_message = []
+for letter in message:
+    encrypted_message.append(monoalpha_cipher.get(letter, letter))
+print(''.join(encrypted_message))
+
