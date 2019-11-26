f72a38f21e546b8e2e5aa4f45747c8753070cf08
Manish Kumar Chaudhary <42311706+chaudharymanishkumar@users.noreply.github.com>
2019-11-26 12:06:07 +0530

    Update client_multichat.py

--- client_multichat.py
+++ client_multichat.py
@@ -1,16 +1,21 @@
 #multichat @client
+#Remove comment from code if you want to add client collapse time 
+#when server is localhost else change ip address of server
+#MKchaudhary
 import socket 
 import sys 
 import time
 from thread import *
 client=socket.socket() 
-client.connect(('127.0.0.1',4448))
-time_started = time.time()
-timeout=10
+port=4448
+client.connect(('127.0.0.1',port))
+#time_started = time.time()
+#timeout=10
 while True:
-	#if time.time() > time_started + timeout:
+	#if time.time() > time_started + timeout:   
 	#	sys.exit()
 	data=client.recv(1024)
 	print "Server: "+data
 	send_data=raw_input("Client: ")
 	client.send(send_data)
+client.close()
