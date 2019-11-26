74d12b66ed5026be45bc7192be98c9dfa3dd7869
Manish Kumar Chaudhary <42311706+chaudharymanishkumar@users.noreply.github.com>
2019-11-26 12:06:36 +0530

    Add files via upload

---
+++ server_multichat.py
@@ -0,0 +1,20 @@
+#multichat @server
+import socket 
+import sys 
+from thread import *
+server=socket.socket() 
+IP_address = ""
+Port = 4448
+server.bind((IP_address, Port)) 
+server.listen(100) 
+
+def create_client(conn):
+	while True:
+		data=raw_input("Server: ")
+		conn.send(data)
+		final=conn.recv(1024)
+		print "Client: "+final
+
+while True:
+	conn,addr=server.accept()
+	start_new_thread(create_client,(conn,))
