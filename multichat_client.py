6c8964eb5a2a1355608d96854a9deab058b1f47b
Manish Kumar Chaudhary <42311706+chaudharymanishkumar@users.noreply.github.com>
2019-11-26 11:54:58 +0530

    Add files via upload

---
+++ multichat_client.py
@@ -0,0 +1,19 @@
+import socket
+import sys
+import select
+client=socket.socket()
+client.connect(('127.0.0.1',4444))
+while True:
+	sockets_list=[sys.stdin,client]
+	read_socket,write_socket,error_socket=select.select(sockets_list,[],[])
+	for sock in read_socket:
+		if sock == client:
+			message=sock.recv(1024)
+			print message
+		else:
+			message=sys.stdin.readline()
+			client.send(message)
+			sys.stdout.write("you: ")
+			sys.stdout.write(message)
+			sys.stdout.flush()
+client.close()
